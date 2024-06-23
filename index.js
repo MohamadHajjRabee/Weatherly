06286c00bd73d7eb119279b9e457d871e69f53cc
MohamadHajjRabee <a39630595@gmail.com>
2024-06-23 11:41:42 +0300

    fixed code for vercel host

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const fetch = require("node-fetch")
 const bodyParser = require("body-parser")
+const path = require('path');
 const ejs = require("ejs")
 const app = express()
 
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
